989119e642eff19a971dea57353249f560d2748d
Evgeniy <zenja099@gmail.com>
2020-09-24 18:40:08 +0300

    Bot is monitoring messages

--- src/send_message.py
+++ src/send_message.py
@@ -1,9 +1,27 @@
-import  requests
+import requests
 
-url = "https://api.telegram.org/bot1162468954:AAEk6dzuhBqfgRm0WO_3QRbZWe0WnYv0_Qs/getUpdates"
-response = requests.get(url)
-data = response.json()
-result = data["result"]
-for update in result:
-    chat_id = update["message"]["chat"]["id"]
-    print(chat_id)
+import datetime
+
+
+def log(message):
+    time_now = datetime.datetime.now()
+    print(f"{time_now} {message}")
+
+
+last_update_id = 0
+
+while True:
+    offset = last_update_id + 1
+    log(f"Sending request. offset = {offset}")
+    url = "https://api.telegram.org/bot1162468954:AAEk6dzuhBqfgRm0WO_3QRbZWe0WnYv0_Qs/getUpdates"
+    response = requests.get(url, params={
+        "offset": offset,
+        "timeout": 5
+    })
+    data = response.json()
+    result = data["result"]
+    for update in result:
+        chat_id = update["message"]["chat"]["id"]
+        message_text = update["message"]["text"]
+        last_update_id = update["update_id"]
+        log(f"Got new message. Chat id = {chat_id}, Message = {message_text}")
