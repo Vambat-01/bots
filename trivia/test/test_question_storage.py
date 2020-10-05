35e706ef2a9c44df9d7c99d82af6d15f07827898
Evgeniy <zenja099@gmail.com>
2020-10-06 00:50:08 +0300

    Create QuestionStorage for Trivia bot

--- trivia/test/test_question_storage.py
+++ trivia/test/test_question_storage.py
@@ -1,11 +1,22 @@
-from trivia.question_storage import add_one
 import unittest
+from trivia.question_storage import JsonQuestionStorage
 
-class AddOneTest(unittest.TestCase):
-    def test_add_one_to_three(self):
-        x = add_one(3)
-        self.assertEqual(4, x)
 
-    def test_add_one_to_four(self):
-        x = add_one(4)
-        self.assertEqual(5, x)
+class JsonQuestionStorageTest(unittest.TestCase):
+    def test_loads_question_correctly(self):
+        json_file = "/home/vambat/projects/bots/trivia/test/resources/test_questions.json"
+        storage = JsonQuestionStorage(json_file)
+        questions = storage.load_questions()
+        self.assertEqual(2, len(questions))
+        quest = questions[0]
+        self.assertEqual("Какое количество цветов у радуги?", quest.text)
+        self.assertEqual(2, quest.points)
+        self.assertEqual(3, len(quest.answers))
+        quest_2 = questions[1]
+        self.assertEqual(22, len(quest_2.text))
+        self.assertEqual(3, quest_2.points)
+        self.assertEqual(2, len(quest_2.answers))
+
+        
+
+
--- trivia/trivia/question_storage.py
+++ trivia/trivia/question_storage.py
@@ -1,12 +1,70 @@
+import json
 from typing import List
+from abc import ABCMeta, abstractmethod
 
 
 class Question:
+    """
+    Представляет собой trivia bot  вопрос
+    """
+
     def __init__(self, text: str, answers: List[str], points: int):
+        """
+        :param text: Текст вопроса, который мы показываем пользователю
+        :param answers: Варианты ответов на вопрос, включая правильный
+        :param points: Количество очков за правильный ответ
+        """
         self.text = text
         self.answers = answers
         self.points = points
 
 
-def add_one(x: int) -> int:
-    return x + 2
+class QuestionStorage(metaclass=ABCMeta):
+    """
+        Интерфейс для доступа к вопросам
+    """
+
+    @abstractmethod
+    def load_questions(self) -> List[Question]:
+        """
+            Считывает список вопросов
+        :return: список вопросов
+        """
+        pass
+
+
+class JsonQuestionStorage(QuestionStorage):
+    """
+        Класс для чтения вопросов из JSON файла
+    """
+
+    def __init__(self, file_path:str):
+        """
+            Передает путь к файлу
+        :param file_path: путь к файлу
+        """
+        self.file_path = file_path
+
+    def load_questions(self) -> List[Question]:
+        """
+            Cчитывает список вопросов из файла
+        :return: список вопросов
+        """
+        with open(self.file_path) as json_file:
+            data = json.load(json_file)
+            questions = []
+            for item in data:
+                text = item["text"]
+                answers = item["answers"]
+                points = item["points"]
+                quest = Question(text, answers, points)
+                questions.append(quest)
+            return questions
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
