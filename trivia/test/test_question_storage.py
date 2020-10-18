fcfbef5314931d6a41b8dd1968624067e3f8deb8
Evgeniy <zenja099@gmail.com>
2020-10-19 02:12:27 +0300

    Fix JsonQuestionStorageTest

--- trivia/test/test_question_storage.py
+++ trivia/test/test_question_storage.py
@@ -7,14 +7,14 @@ class JsonQuestionStorageTest(unittest.TestCase):
         json_file = "resources/test_questions.json"
         storage = JsonQuestionStorage(json_file)
         questions = storage.load_questions()
-        self.assertEqual(2, len(questions))
+        self.assertEqual(3, len(questions))
         quest = questions[0]
-        self.assertEqual("Какое количество цветов у радуги?", quest.text)
-        self.assertEqual(2, quest.points)
-        self.assertEqual(3, len(quest.answers))
+        self.assertEqual("7+3", quest.text)
+        self.assertEqual(1, quest.points)
+        self.assertEqual(2, len(quest.answers))
         quest_2 = questions[1]
-        self.assertEqual(22, len(quest_2.text))
-        self.assertEqual(3, quest_2.points)
+        self.assertEqual(4, len(quest_2.text))
+        self.assertEqual(2, quest_2.points)
         self.assertEqual(2, len(quest_2.answers))
 
         
