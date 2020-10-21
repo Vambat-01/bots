2013e427b264fcdbc89d3e555f44d60e2ab9080f
Evgeniy <zenja099@gmail.com>
2020-10-21 19:07:50 +0300

    Create interface TelegramApi and RealTelegramApi

--- trivia/main.py
+++ trivia/main.py
@@ -1,7 +1,14 @@
-from trivia.bot import Bot
+from trivia.bot import Bot, RealTelegramApi
+from trivia.bot_state import BotStateFactory, GreetingState
+from  trivia.question_storage import JsonQuestionStorage
 
 token = "1162468954:AAEk6dzuhBqfgRm0WO_3QRbZWe0WnYv0_Qs"
-bot = Bot(token)
+json_file = "resources/questions_for_bot.json"
+storage = JsonQuestionStorage(json_file)
+state_factory = BotStateFactory(storage)
+state = GreetingState(state_factory)
+telegram_api = RealTelegramApi(token)
+bot = Bot(telegram_api, state)
 
 while True:
     bot.process_updates()
--- trivia/trivia/bot.py
+++ trivia/trivia/bot.py
@@ -1,8 +1,8 @@
 import requests
 import datetime
-from trivia.bot_state import EchoState, Message, Command, BotState, GreetingState, BotStateFactory
-from trivia.question_storage import JsonQuestionStorage
-import os
+from trivia.bot_state import Message, Command, BotState
+from requests.models import Response
+from abc import ABCMeta, abstractmethod
 
 
 def log(message: str) -> None:
@@ -15,27 +15,77 @@ def log(message: str) -> None:
     print(f"{time_now} {message}")
 
 
-class Bot:
-    def __init__(self, token: str):
-        json_file = "resources/questions_for_bot.json"
-        storage = JsonQuestionStorage(json_file)
-        state_factory = BotStateFactory(storage)
-        self.token = token
-        self.last_update_id = 0
-        self.state: BotState = GreetingState(state_factory)
+class TelegramApi(metaclass=ABCMeta):
+    """
+        Интерфейс получение входящих обновлений и отправки сообщений в телеграм
+    """
 
-    def process_updates(self) -> None:
+    @abstractmethod
+    def get_updates(self, offset: int) -> Response:
         """
-            Получает входящее обновление
+            Получение входящего обновления
+        """
+        pass
+
+    @abstractmethod
+    def send_message(self, chat_id: int, text: str) -> None:
+        """
+            Отправляет текстовое сообщение
+        :param chat_id: идентификация чата
+        :param text: текст сообщения
         :return: None
         """
+        pass
+
+
+class RealTelegramApi(TelegramApi):
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_updates(self, offset: int) -> Response:
+        """
+            Получает входящее обновление
+        :param offset: числовой номер обновления
+        :return: Response
+        """
         url = f"https://api.telegram.org/bot{self.token}/getUpdates"
-        offset = self.last_update_id + 1
         log(f"Listen to telegram. offset: {offset}")
         response = requests.get(url, params={
             "offset": offset,
             "timeout": 10
         })
+        return response
+
+    def send_message(self, chat_id: int, text: str) -> None:
+        """
+            Отправляет текстовое сообщение
+        :param chat_id: идентификатор чата
+        :param text: текст сообщения
+        :return: None
+        """
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        response = requests.get(url, params={
+            "text": text,
+            "chat_id": str(chat_id)
+        })
+        log(f"Send message status code: {response.status_code} ")
+
+
+class Bot:
+    """
+        Обрабатывает полученные команды и сообщения от пользователя
+    """
+    def __init__(self, telegram_api: TelegramApi, state: BotState):
+        self.telegram_api = telegram_api
+        self.state = state
+        self.last_update_id = 0
+
+    def process_updates(self) -> None:
+        """
+           Обрабатывает полученные команды и сообщения от пользователя
+        :return: None
+        """
+        response = self.telegram_api.get_updates(self.last_update_id + 1)
         data = response.json()
         result = data["result"]
         for update in result:
@@ -49,28 +99,14 @@ class Bot:
                 user_message = Message(chat_id, message_text)
                 bot_response = self.state.process_message(user_message)
             self.last_update_id = update["update_id"]
-            self.send_message(bot_response.message.chat_id, bot_response.message.text)
+            self.telegram_api.send_message(bot_response.message.chat_id, bot_response.message.text)
 
             if bot_response.new_state is not None:
                 new_state: BotState = bot_response.new_state
                 self.state = new_state
                 first_message = self.state.on_enter(chat_id)
                 if first_message is not None:
-                    self.send_message(first_message.chat_id, first_message.text)
-
-    def send_message(self, chat_id: int, text: str ) -> None:
-        """
-            Отправляет текстовое сообщение
-        :param chat_id: идентификатор чата
-        :param text: текст сообщения
-        :return:None
-        """
-        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
-        response = requests.get(url, params={
-            "text": text,
-            "chat_id": str(chat_id)
-        })
-        log(f"Send message status code: {response.status_code} ")
+                    self.telegram_api.send_message(first_message.chat_id, first_message.text)
 
 
 
