202f96ba10b526f40cc68ed55ac1cbd916e7b805
Evgeniy <zenja099@gmail.com>
2020-11-01 03:35:37 +0300

    Fix test_format

--- trivia/test/test_format.py
+++ trivia/test/test_format.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from trivia import format
 import textwrap
 from trivia.question_storage import Question
+from trivia.utils import dedent_and_strip
 
 
 class GetNumbersOfAnswersHelpTest(TestCase):
@@ -12,37 +13,39 @@ class GetNumbersOfAnswersHelpTest(TestCase):
 
 
 class GetResponseForValidAnswerText(TestCase):
-    def test_get_response_is_correct(self):
-        check_1 = textwrap.dedent(
-            """
-                <i>&#127774 Answer is correct!</i> <b>&#10067Next question:</b>
-                    <b>10+10</b>
-                <i>Choose answer:</i>
-                1: 20
-                2: 45
-            """
-        ).strip()
-        self.assertEqual(check_1, format.get_response_for_valid_answer(True, Question("10+10", ["20", "45"], 0)))
+    def test_when_answer_is_correct_and_has_next_question(self):
+        expected_text = """
+                    <i>&#127774 Answer is correct!</i> <b>&#10067Next question:</b>
+                        <b>10+10</b>
+                    <i>Choose answer:</i>
+                    1: 20
+                    2: 45
+                 """
 
-    def test_get_response_is_not_correct(self):
-        check_1 = textwrap.dedent(
-            """
-                <i>&#127783 Answer is not correct!</i> <b>&#10067Next question:</b>
-                    <b>15+10</b>
-                <i>Choose answer:</i>
-                1: 30
-                2: 28
-            """
-        ).strip()
-        self.assertEqual(check_1, format.get_response_for_valid_answer(False, Question("15+10", ["30", "28"], 0)))
+        self.assertEqual(dedent_and_strip(expected_text),
+                         format.get_response_for_valid_answer(True, Question("10+10", ["20", "45"], 0))
+                         )
 
-    def test_get_response_is_correct_score(self):
-        check_1 = "<i>&#127774 Answer is correct!</i> <i>The game is over. Your points: 6</i>"
-        self.assertEqual(check_1, format.get_response_for_valid_answer(True, game_score=6))
+    def test_when_answer_is_not_correct_and_has_next_question(self):
+        expected_text = """
+                        <i>&#127783 Answer is not correct!</i> <b>&#10067Next question:</b>
+                            <b>15+10</b>
+                        <i>Choose answer:</i>
+                        1: 30
+                        2: 28
+                     """
 
-    def test_get_response_is_not_correct_score(self):
-        check_1 = "<i>&#127783 Answer is not correct!</i> <i>The game is over. Your points: 2</i>"
-        self.assertEqual(check_1, format.get_response_for_valid_answer(False, game_score=2))
+        self.assertEqual(dedent_and_strip(expected_text),
+                         format.get_response_for_valid_answer(False, Question("15+10", ["30", "28"], 0))
+                        )
+
+    def test_when_answer_is_correct_and_score(self):
+        expected_text = "<i>&#127774 Answer is correct!</i> <i>The game is over. Your points: 6</i>"
+        self.assertEqual(expected_text, format.get_response_for_valid_answer(True, game_score=6))
+
+    def test_when_answer_is_not_correct_and_score(self):
+        expected_text = "<i>&#127783 Answer is not correct!</i> <i>The game is over. Your points: 2</i>"
+        self.assertEqual(expected_text, format.get_response_for_valid_answer(False, game_score=2))
 
 
 
