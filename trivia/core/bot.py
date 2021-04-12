ea0b34addc76bbb3c5bb8b470bf6bd33267be28f
Evgeniy <zenja099@gmail.com>
2021-04-13 01:47:24 +0300

    bot is working as a client

--- trivia/core/bot.py
+++ trivia/core/bot.py
@@ -10,7 +10,7 @@ from trivia.bijection import Bijection
 from dataclasses import dataclass, field
 from dataclasses_json import dataclass_json
 from core.utils import JsonDict
-from trivia.telegram_models import Update
+from trivia.telegram_models import Update, UpdatesResponse
 
 
 class Bot:
@@ -67,44 +67,44 @@ class Bot:
                     state = {self.state}
                 """
 
-    def process_updates(self) -> None:
+    def process_updates(self, update: Update) -> None:
         """
            Обрабатывает полученные команды и сообщения от пользователя
         :return: None
         """
-        response = self.telegram_api.get_updates(self.state.last_update_id + 1)
-        result = response.result
-        for update in result:
-            self.state.last_update_id = update.update_id
-            chat_id = update.get_chat_id(update)
-            state = self._get_state_for_chat(chat_id)
-            bot_response = self.process_update(update, state)
-            if bot_response is not None:
-                if bot_response.message is not None:
-                    self.telegram_api.send_message(bot_response.message.chat_id,
-                                                   bot_response.message.text,
-                                                   bot_response.message.parse_mode,
-                                                   bot_response.message.keyboard
-                                                   )
+        # response = self.telegram_api.get_updates(self.state.last_update_id + 1)
+        # result = response.result
+        # for update in result:
+        #     self.state.last_update_id = update.update_id
+        chat_id = update.get_chat_id(update)
+        state = self._get_state_for_chat(chat_id)
+        bot_response = self.process_update(update, state)
+        if bot_response is not None:
+            if bot_response.message is not None:
+                self.telegram_api.send_message(bot_response.message.chat_id,
+                                               bot_response.message.text,
+                                               bot_response.message.parse_mode,
+                                               bot_response.message.keyboard
+                                               )
 
-                if bot_response.message_edit is not None:
-                    self.telegram_api.edit_message(bot_response.message_edit.chat_id,
-                                                   bot_response.message_edit.message_id,
-                                                   bot_response.message_edit.text,
-                                                   bot_response.message_edit.parse_mode
-                                                   )
+            if bot_response.message_edit is not None:
+                self.telegram_api.edit_message(bot_response.message_edit.chat_id,
+                                               bot_response.message_edit.message_id,
+                                               bot_response.message_edit.text,
+                                               bot_response.message_edit.parse_mode
+                                               )
 
-                if bot_response.new_state is not None:
-                    new_state: BotState = bot_response.new_state
-                    wrapped_new_state = BotStateLoggingWrapper(new_state)
-                    self.state.chat_states[chat_id] = wrapped_new_state
-                    first_message = wrapped_new_state.on_enter(chat_id)
-                    if first_message is not None:
-                        self.telegram_api.send_message(first_message.chat_id,
-                                                       first_message.text,
-                                                       first_message.parse_mode,
-                                                       first_message.keyboard
-                                                       )
+            if bot_response.new_state is not None:
+                new_state: BotState = bot_response.new_state
+                wrapped_new_state = BotStateLoggingWrapper(new_state)
+                self.state.chat_states[chat_id] = wrapped_new_state
+                first_message = wrapped_new_state.on_enter(chat_id)
+                if first_message is not None:
+                    self.telegram_api.send_message(first_message.chat_id,
+                                                   first_message.text,
+                                                   first_message.parse_mode,
+                                                   first_message.keyboard
+                                                   )
 
     def process_update(self, update: Update, state: BotState) -> Optional[BotResponse]:
         if update.message:
