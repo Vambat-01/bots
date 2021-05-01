51d61022ee5df5899e518bc9264bdec147ee3e26
Evgeniy <zenja099@gmail.com>
2021-05-01 03:03:12 +0300

    fix

--- trivia/core/live_redis_api.py
+++ trivia/core/live_redis_api.py
@@ -6,7 +6,10 @@ from trivia.bot_config import LiveRedisApiConfig
 
 
 class LockChatException(Exception):
-    def __init__(self, chat_id: int, max_attempts):
+    """
+    Класс вызова исключения для Redis
+    """
+    def __init__(self, chat_id: int, max_attempts: int):
         super().__init__()
         self.chat_id = chat_id
         self.max_attempts = max_attempts
@@ -17,20 +20,20 @@ class LockChatException(Exception):
 
 class LiveRedisApi(RedisApi):
     def __init__(self, config: LiveRedisApiConfig):
-        self.config = config
+        self._config = config
         self._redis = redis.Redis(host=config.host, port=config.port, db=0)
 
     def close(self):
         self._redis.close()
 
     async def lock_chat(self, chat_id: int) -> None:
-        for _ in range(self.config.max_attempts):
-            was_set = self._redis.set(str(chat_id), "1", ex=self.config.expire_sec, nx=True)
+        for _ in range(self._config.max_attempts):
+            was_set = self._redis.set(str(chat_id), "1", ex=self._config.expire_sec, nx=True)
             if was_set:
                 return
-            await asyncio.sleep(self.config.delay_ms / 1000)
+            await asyncio.sleep(self._config.delay_ms / 1000)
 
-        raise LockChatException(chat_id, self.config.max_attempts)
+        raise LockChatException(chat_id, self._config.max_attempts)
 
     def unlock_chat(self, chat_id: int) -> None:
         self._redis.delete(str(chat_id))
